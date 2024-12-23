96ade2d745fddd3f261d396062781735ecc25f55
sanikadeore <sanikadeore2005@gmail.com>
2024-12-23 12:56:40 +0530

    Update user.js

--- main/models/user.js
+++ main/models/user.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false }, // Verification status
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  cart: { type: mongoose.Schema.Types.ObjectId, ref: 'Cart' }, // Reference to the user's cart
+  orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }], // List of user orders
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
