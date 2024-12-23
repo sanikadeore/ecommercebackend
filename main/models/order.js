8c60176d0a16674bfe5f305421a774c7e4f048a7
sanikadeore <sanikadeore2005@gmail.com>
2024-12-23 12:57:15 +0530

    Update order.js

--- main/models/order.js
+++ main/models/order.js
@@ -1,1 +1,22 @@
+const mongoose = require('mongoose');
 
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  products: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+      quantity: { type: Number, required: true },
+      price: { type: Number, required: true },
+      discount: { type: Number, default: 0 },
+      total: { type: Number, required: true }
+    }
+  ],
+  totalAmount: { type: Number, required: true },
+  discount: { type: Number, default: 0 },
+  deliveryCharges: { type: Number, default: 0 },
+  status: { type: String, default: 'pending' }, // Order status (pending, shipped, delivered)
+  createdAt: { type: Date, default: Date.now },
+  shippingAddress: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
