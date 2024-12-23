9411dfa9bd99e72105383132f9a8bb2b7b607f0f
sanikadeore <sanikadeore2005@gmail.com>
2024-12-23 13:22:08 +0530

    Update orderRoutes.js

--- main/routes/orderRoutes.js
+++ main/routes/orderRoutes.js
@@ -1,1 +1,10 @@
+// routes/orderRoutes.js
+const express = require('express');
+const router = express.Router();
+const { placeOrder } = require('../controllers/orderController');
+const { verifyUser } = require('../middlewares/authMiddleware');  // Ensure user is authenticated
 
+// Place an order after validating cart
+router.post('/placeOrder', verifyUser, placeOrder);
+
+module.exports = router;
