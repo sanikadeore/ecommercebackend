8429c393d88995fff949b43f906f4eb7ed72e462
sanikadeore <sanikadeore2005@gmail.com>
2024-12-23 12:59:33 +0530

    Update complaint.js

--- main/models/complaint.js
+++ main/models/complaint.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const complaintSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' }, // Optional field to link to an order
+  orderId: { type: mongoose.Schema.Types.ObjectId, ref: 'Order' }, 
   description: { type: String, required: true },
   status: { type: String, default: 'open' }, // Complaint status (open, in progress, resolved)
   createdAt: { type: Date, default: Date.now }
