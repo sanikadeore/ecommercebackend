dd84abdeed23297da20bf8bc72e93cb20711ca82
sanikadeore <sanikadeore2005@gmail.com>
2024-12-23 13:01:15 +0530

    Update product.js

--- main/models/product.js
+++ main/models/product.js
@@ -5,14 +5,14 @@ const productSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   description: { type: String, required: true },
   category: { type: String, required: true },
-  productId: { type: String, unique: true, required: true }, // Unique product ID
-  inStockValue: { type: Number, default: 0 }, // Available stock value
-  soldStockValue: { type: Number, default: 0 }, // Number of items sold
-  visibility: { type: String, default: 'on' }, // Visibility of product
-  images: [{ type: String }], // Array of image paths
-  freeDelivery: { type: Boolean, default: false }, // Flag for free delivery
-  discount: { type: Number, default: 0 }, // Discount percentage
-  sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Seller', required: true } // Seller ID reference
+  productId: { type: String, unique: true, required: true }, 
+  inStockValue: { type: Number, default: 0 }, 
+  soldStockValue: { type: Number, default: 0 }, 
+  visibility: { type: String, default: 'on' }, 
+  images: [{ type: String }], 
+  freeDelivery: { type: Boolean, default: false }, 
+  discount: { type: Number, default: 0 }, 
+  sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Seller', required: true } 
 });
 
 module.exports = mongoose.model('Product', productSchema);
