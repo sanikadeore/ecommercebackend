00cfa78729be0db62ae96a0dd9ac851380b2712e
sanikadeore <sanikadeore2005@gmail.com>
2024-12-23 12:56:08 +0530

    Update seller.js

--- main/models/seller.js
+++ main/models/seller.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
 
+const sellerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  address: { type: String, required: true },
+  verificationStatus: { type: String, default: 'pending' }, // Verification status (pending, verified)
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Seller', sellerSchema);
